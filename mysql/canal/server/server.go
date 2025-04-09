3dd612753ab7ad385eb6402c2aef371234335418
flc1125 <four_leaf_clover@foxmail.com>
2025-04-09 13:51:25 +0800

    refactor(canal-server): update canal package references in server code and go.mod

--- mysql/canal/server/server.go
+++ mysql/canal/server/server.go
@@ -4,25 +4,25 @@ import (
 	"context"
 
 	"github.com/go-fries/fries/contract/v3"
-	canal2 "github.com/go-fries/fries/mysql/canal/v3"
+	"github.com/go-fries/fries/mysql/canal/v3"
 )
 
 type Server struct {
-	canal *canal2.Canal
+	canal *canal.Canal
 }
 
 var _ contract.Server = (*Server)(nil)
 
-func NewCanalServer(canal *canal2.Canal) *Server {
+func NewCanalServer(canal *canal.Canal) *Server {
 	return &Server{
 		canal: canal,
 	}
 }
 
 func (s *Server) Start(ctx context.Context) error {
-	return s.Start(ctx)
+	return s.canal.Start(ctx)
 }
 
 func (s *Server) Stop(ctx context.Context) error {
-	return s.Stop(ctx)
+	return s.canal.Stop(ctx)
 }
