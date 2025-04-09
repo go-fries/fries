9f5930a46645eb18752ef120333aa1ac599b6727
flc1125 <four_leaf_clover@foxmail.com>
2025-04-09 14:19:43 +0800

    refactor(canal-server): rename NewCanalServer to New and add RegisterListeners method

--- mysql/canal/server/server.go
+++ mysql/canal/server/server.go
@@ -3,26 +3,29 @@ package server
 import (
 	"context"
 
-	"github.com/go-fries/fries/contract/v3"
 	"github.com/go-fries/fries/mysql/canal/v3"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/transport"
 )
 
 type Server struct {
 	canal *canal.Canal
 }
 
-var _ contract.Server = (*Server)(nil)
+var _ transport.Server = (*Server)(nil)
 
-func NewCanalServer(canal *canal.Canal) *Server {
+func New(canal *canal.Canal) *Server {
 	return &Server{
 		canal: canal,
 	}
 }
 
 func (s *Server) Start(ctx context.Context) error {
+	log.Infof("[Canal] server starting")
 	return s.canal.Start(ctx)
 }
 
 func (s *Server) Stop(ctx context.Context) error {
+	log.Infof("[Canal] server stopping")
 	return s.canal.Stop(ctx)
 }
