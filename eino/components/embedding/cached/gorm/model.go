82072e58a889d733e10cb37a892b1b1959a907dc
flc1125 <four_leaf_clover@foxmail.com>
2025-06-05 18:51:35 +0800

    test(eino/components/embedding/cached/gorm): add unit tests for Model
    - Add testify package for unit testing
    - Remove unnecessary GORM tags in model fields
    - Implement unit tests for Model's TableName method

--- eino/components/embedding/cached/gorm/model.go
+++ eino/components/embedding/cached/gorm/model.go
@@ -9,9 +9,9 @@ import (
 var tableName = "embeddings_cache"
 
 type Model struct {
-	Key     string                        `gorm:"column:key;primaryKey;size:255;not null;comment:'Key'" json:"key"`
-	Vector  datatypes.JSONType[[]float64] `gorm:"column:vector;type:json;not null;comment:'Vector'" json:"vector"`
-	Expired time.Time                     `gorm:"column:expired;type:datetime;not null;comment:'Expired'" json:"expired"`
+	Key     string                        `gorm:"column:key;primaryKey" json:"key"`
+	Vector  datatypes.JSONType[[]float64] `gorm:"column:vector;type:json" json:"vector"`
+	Expired time.Time                     `gorm:"column:expired;type:datetime" json:"expired"`
 }
 
 func (*Model) TableName() string {
