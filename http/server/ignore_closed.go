17449328cc33ee6353763d1cad7eaeff3599fc1a
Flc <i@flc.io>
2026-07-08 19:52:24 +0800

    fix(http): export server closed wrapper type

--- http/server/ignore_closed.go
+++ http/server/ignore_closed.go
@@ -7,15 +7,21 @@ import (
 )
 
 // IgnoreServerClosed returns a server that treats [http.ErrServerClosed] from Start as nil.
-func IgnoreServerClosed(srv server) server {
-	return ignoreClosed{srv: srv}
+func IgnoreServerClosed(srv interface {
+	Start(context.Context) error
+	Stop(context.Context) error
+},
+) IgnoreClosed {
+	return IgnoreClosed{srv: srv}
 }
 
-type ignoreClosed struct {
+// IgnoreClosed wraps a server and ignores [http.ErrServerClosed] returned by Start.
+type IgnoreClosed struct {
 	srv server
 }
 
-func (s ignoreClosed) Start(ctx context.Context) error {
+// Start starts the wrapped server and returns nil for [http.ErrServerClosed].
+func (s IgnoreClosed) Start(ctx context.Context) error {
 	err := s.srv.Start(ctx)
 	if errors.Is(err, http.ErrServerClosed) {
 		return nil
@@ -23,6 +29,7 @@ func (s ignoreClosed) Start(ctx context.Context) error {
 	return err
 }
 
-func (s ignoreClosed) Stop(ctx context.Context) error {
+// Stop stops the wrapped server.
+func (s IgnoreClosed) Stop(ctx context.Context) error {
 	return s.srv.Stop(ctx)
 }
--- http/server/ignore_closed_test.go
+++ http/server/ignore_closed_test.go
@@ -1,4 +1,4 @@
-package server
+package server_test
 
 import (
 	"context"
@@ -6,13 +6,14 @@ import (
 	"net/http"
 	"testing"
 
+	server "github.com/go-fries/fries/http/server/v4"
 	"github.com/stretchr/testify/require"
 )
 
 func TestIgnoreServerClosedSuppressesHTTPServerClosed(t *testing.T) {
 	srv := &fakeServer{startErr: http.ErrServerClosed}
 
-	err := IgnoreServerClosed(srv).Start(t.Context())
+	err := server.IgnoreServerClosed(srv).Start(t.Context())
 
 	require.NoError(t, err)
 }
@@ -21,7 +22,7 @@ func TestIgnoreServerClosedPreservesOtherStartErrors(t *testing.T) {
 	startErr := errors.New("listen failed")
 	srv := &fakeServer{startErr: startErr}
 
-	err := IgnoreServerClosed(srv).Start(t.Context())
+	err := server.IgnoreServerClosed(srv).Start(t.Context())
 
 	require.ErrorIs(t, err, startErr)
 }
@@ -30,7 +31,7 @@ func TestIgnoreServerClosedDelegatesStop(t *testing.T) {
 	stopErr := errors.New("stop failed")
 	srv := &fakeServer{stopErr: stopErr}
 
-	err := IgnoreServerClosed(srv).Stop(t.Context())
+	err := server.IgnoreServerClosed(srv).Stop(t.Context())
 
 	require.ErrorIs(t, err, stopErr)
 	require.True(t, srv.stopped)
